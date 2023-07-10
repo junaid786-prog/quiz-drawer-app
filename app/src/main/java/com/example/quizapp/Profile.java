506fa55d30ab18ad4ae6fbb43ae1da1df46df6f6
junaid786-prog <junaidshoaib555@gmail.com>
2023-07-10 11:22:54 +0500

    github button added

--- app/src/main/java/com/example/quizapp/Profile.java
+++ app/src/main/java/com/example/quizapp/Profile.java
@@ -1,5 +1,7 @@
 package com.example.quizapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +18,8 @@ import android.view.ViewGroup;
  */
 public class Profile extends Fragment {
 
+    View view;
+    Button button;
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -58,7 +63,20 @@ public class Profile extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        view =  inflater.inflate(R.layout.fragment_profile, container, false);
+        button = view.findViewById(R.id.visitGithubBtn);
+
+        // Set the click listener for the button
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                String url = "https://github.com/junaid786-prog/quiz-drawer-app";  // Replace with your desired URL
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+        return view;
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 }
