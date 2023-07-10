f9ab1ffbfbfd059444737a2161947ee8b6a3294c
junaid786-prog <junaidshoaib555@gmail.com>
2023-07-10 08:24:01 +0500

    on click listener added to menu icon

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.quizapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toolbar;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Inside your activity's onCreate() method
+        // Inside your activity's onCreate() method
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        MaterialToolbar toolbar = findViewById(R.id.toolbar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
     }
 }
